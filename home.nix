6b282e4492ed36c1944233f84798511b96ca6646
Felix Hirner <mail@felix-hirner.de>
2025-11-29 14:10:53 +0100

    feat: add ssh config

--- home.nix
+++ home.nix
@@ -18,5 +18,14 @@
     };
     home-manager.enable = true;
     helix.enable = true;
+    ssh = {
+      enable = true;
+      enableDefaultConfig = false;
+      matchBlocks = {
+        "*" = {
+          identityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
+        };
+      };
+    };
   };
 }
