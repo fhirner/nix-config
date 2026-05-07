2e618fad643cb98a1c5666af7be480f0b51aea69
Felix Hirner <git@futix.eu>
2026-05-07 16:38:12 +0200

    feat: add yabai config

--- darwin/yabai.nix
+++ darwin/yabai.nix
@@ -16,5 +16,14 @@
       mouse_action2 = "resize";
       layout = "bsp";
     };
+    extraConfig = ''
+      yabai -m rule --add app="^System Settings$" manage=off
+      yabai -m rule --add app="^SteerMouse$" manage=off
+      yabai -m rule --add title="^Preferences$" manage=off
+      yabai -m rule --add app="^FaceTime$" manage=off
+      yabai -m rule --add app="Stickies" manage=off
+      yabai -m rule --add app="^Cryptomator$" manage=off
+      yabai -m signal --add event=window_created action="yabai -m query --windows --window $YABAI_WINDOW_ID | jq -e '.\"can-resize\"' || yabai -m window $YABAI_WINDOW_ID --toggle float"
+    '';
   };
 }
