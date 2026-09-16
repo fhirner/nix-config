4afe35df795c4440f2ef2188db086c79080c53e7
Felix Hirner <git@futix.eu>
2026-09-16 16:49:46 +0200

    fix: escape path

--- home/default.nix
+++ home/default.nix
@@ -34,7 +34,7 @@
       enable = true;
       enableDefaultConfig = false;
       settings."*" = {
-        IdentityAgent = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+        IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
       };
     };
     mise = {
